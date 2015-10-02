064701319ee1240392d93e75bdacd03087a66b7f
Mike Wakeland <mike_wakeland@craftsamericana.com>
2015-10-02 11:10:49 -0700

    added user check for uploads

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -98,37 +98,43 @@ class ArticleController extends Controller{
     }
     public function getMedia()
     {
-      $menuItems = $this->buildMenu();
-      return view('article/media', ['menuItems' => $menuItems]);
+      if (Auth::user()->name == 'Mike Wakeland')
+      {
+        $menuItems = $this->buildMenu();
+        return view('article/media', ['menuItems' => $menuItems]);
+      }
 
     }
     public function uploadMedia()
     {
-      if(isset($_FILES['image'])){
-        $errors= array();
-        $file_name = $_FILES['image']['name'];
-        $file_size =$_FILES['image']['size'];
-        $file_tmp =$_FILES['image']['tmp_name'];
-        $file_type=$_FILES['image']['type'];
-        /*$file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
-        $extensions = array("jpeg","jpg","png");
+      if (Auth::user()->name == 'Mike Wakeland')
+      {
 
-        if(in_array($file_ext,$extensions )=== false){
-          $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-        }*/
+        if(isset($_FILES['image'])){
+          $errors= array();
+          $file_name = $_FILES['image']['name'];
+          $file_size =$_FILES['image']['size'];
+          $file_tmp =$_FILES['image']['tmp_name'];
+          $file_type=$_FILES['image']['type'];
+          /*$file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+          $extensions = array("jpeg","jpg","png");
 
-        if($file_size > 2097152){
-          $errors[]='File size must be excately 2 MB';
-        }
+          if(in_array($file_ext,$extensions )=== false){
+            $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+          }*/
 
-        if(empty($errors)==true){
-          move_uploaded_file($file_tmp, "/home/forge/default/public/img/" . $file_name);
-          echo "Success.\nFile URL: /img/".$file_name;
-        }else{
-          print_r($errors);
+          if($file_size > 2097152){
+            $errors[]='File size must be excately 2 MB';
+          }
+
+          if(empty($errors)==true){
+            move_uploaded_file($file_tmp, "/home/forge/default/public/img/" . $file_name);
+            echo "Success.\nFile URL: /img/".$file_name;
+          }else{
+            print_r($errors);
+          }
         }
       }
-
     }
 
     //*****************
