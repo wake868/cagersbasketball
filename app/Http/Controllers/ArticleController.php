aa02c851d1d77c1cd5784e2dea79aa4e692966af
Mike Wakeland <mike_wakeland@craftsamericana.com>
2015-10-02 10:54:05 -0700

    changes to the media blade template

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -110,12 +110,12 @@ class ArticleController extends Controller{
         $file_size =$_FILES['image']['size'];
         $file_tmp =$_FILES['image']['tmp_name'];
         $file_type=$_FILES['image']['type'];
-        $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+        /*$file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
         $extensions = array("jpeg","jpg","png");
 
         if(in_array($file_ext,$extensions )=== false){
           $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-        }
+        }*/
 
         if($file_size > 2097152){
           $errors[]='File size must be excately 2 MB';
