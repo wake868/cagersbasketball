8aa08c36db9b4332a3255ab3c56f19c5a65e4489
Mike Wakeland <mike_wakeland@craftsamericana.com>
2015-10-02 11:02:28 -0700

    changes to the media blade template

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -122,7 +122,7 @@ class ArticleController extends Controller{
         }
 
         if(empty($errors)==true){
-          move_uploaded_file($file_tmp, '../../../public/img/'.$file_name);
+          move_uploaded_file($file_tmp, "/home/forge/default/public/img/" . $file_name);
           echo "Success";
         }else{
           print_r($errors);
