2e5a81a816f13037dd24e71d2d454961ae5239ba
Mike Wakeland <mike_wakeland@craftsamericana.com>
2015-10-02 10:49:17 -0700

    changes to the media blade template

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -102,16 +102,32 @@ class ArticleController extends Controller{
       return view('article/media', ['menuItems' => $menuItems]);
 
     }
-    public function uploadMedia(Request $request)
+    public function uploadMedia()
     {
-      $file = Input::file('image');
-      $destinationPath = 'upload/';
-      $filename = $file->getClientOriginalName();
-      //Input::file('image')->move($destinationPath, $filename);
+      if(isset($_FILES['image'])){
+        $errors= array();
+        $file_name = $_FILES['image']['name'];
+        $file_size =$_FILES['image']['size'];
+        $file_tmp =$_FILES['image']['tmp_name'];
+        $file_type=$_FILES['image']['type'];
+        $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+        $extensions = array("jpeg","jpg","png");
 
+        if(in_array($file_ext,$extensions )=== false){
+          $errors[]="extension not allowed, please choose a JPEG or PNG file."
+        }
 
-      dd($filename);
+        if($file_size > 2097152){
+          $errors[]='File size must be excately 2 MB';
+        }
 
+        if(empty($errors)==true){
+          move_uploaded_file($file_tmp,"/public/img/".$file_name);
+          echo "Success";
+        }else{
+          print_r($errors);
+        }
+      }
 
     }
 
