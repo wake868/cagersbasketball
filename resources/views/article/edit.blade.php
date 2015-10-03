2884a04df7f2e58a0f3ad03a77e85af7779306b2
Mike Wakeland <mike_wakeland@craftsamericana.com>
2015-10-02 21:05:20 -0700

    added popover on edit page

--- resources/views/article/edit.blade.php
+++ resources/views/article/edit.blade.php
@@ -58,8 +58,8 @@
             </div>
             <div class="form-group">
                 <label for="content"><b>Content</b></label>
-                <button type="button" class="btn btn-info" data-container="body" data-toggle="popover" data-placement="bottom" style="margin-left:15px;width:500px;"
-                data-content="@foreach($mediaFiles as $media)<img src='/img/{{$media}}' width='50' height='50'>&nbsp;&nbsp;&nbsp;/img/{{$media}}<br />@endforeach">
+                <button type="button" class="btn btn-info" data-container="body" data-toggle="popover" data-placement="bottom" style="margin-left:15px;"
+                data-content="@foreach($mediaFiles as $media)<img src='/img/{{$media}}' width='25' height='25'>&nbsp;&nbsp;&nbsp;/img/{{$media}}<br />@endforeach">
                   View Uploaded Media Files
                 </button>
                 <textarea name="content" class="form-control" id="content" rows="250"><?php echo html_entity_decode($article->content); ?></textarea>
