746a79e79ea9f25891cd09464402a74599df6159
Mike Wakeland <mike_wakeland@craftsamericana.com>
2015-10-02 21:09:02 -0700

    added popover on edit page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -98,7 +98,7 @@
         <script type="text/javascript">
           $(document).ready(function () {
             $(function () {
-              $('[data-toggle="popover"]').popover({html:true})
+              $('[data-toggle="popover"]').popover({html:true, container:'body'})
             });
           });
         </script>
